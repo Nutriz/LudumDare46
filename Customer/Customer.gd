a3bfa7069d70c2609df7c4e0529d9803f0f12853
Nutriz <jerome.gully0@gmail.com>
2020-04-18 22:54:43 +0200

    Moving customers to tables (WIP)

--- Customer/Customer.gd
+++ Customer/Customer.gd
@@ -2,9 +2,11 @@ extends KinematicBody
 
 var gravity = Vector3.DOWN * 10
 var vel = Vector3()
-export var speed = 5
-export var rotate_speed = 0.1
-export var MAX_SPEED = 4
+export var speed = 3
+
+
+var path
+var path_ind = 0
 
 var happiness = 3 # 1 = sad, 3 = happy
 var hungry = 2    # 1 = not hungry, 4 = very hungry
@@ -19,12 +21,25 @@ func _process(delta):
 
 func _physics_process(delta):
 	vel += gravity * delta
-	if target_x != null and translation.x <= target_x:
+
+	if path != null:
+		if path_ind < path.size():
+			var look_to = Vector3(path[path_ind])
+			look_at(path[path_ind], Vector3.UP)
+			var move_vec = (path[path_ind] - global_transform.origin)
+			if move_vec.length() < 0.1:
+				path_ind += 1
+			else:
+				move_and_slide(move_vec.normalized() * speed, Vector3.UP)
+		else:
+			rotate_y(-90)
+			path = null
+	elif target_x != null and translation.x <= target_x:
 		vel.x += 0.05
+		vel = move_and_slide(vel, Vector3.UP)
 	else:
 		vel.x = 0
-
-	vel = move_and_slide(vel, Vector3.UP)
+		target_x = null
 
 func move_two_unit():
 	target_x = translation.x + 2
--- Game.gd
+++ Game.gd
@@ -45,14 +45,20 @@ func display_dishes_popup():
 	$UI/DishesPopup.set_position(get_popup_position())
 	$UI/DishesPopup.visible = true
 
+
+func _on_OrderMenu_confirmed():
+	var current_customer = $Customers.get_child(0)
+	current_customer.path = $Navigation.get_simple_path(current_customer.global_transform.origin, $Tables/Table1.translation)
+
+	$Customers.remove_child(current_customer)
+	$TableCustomers.add_child(current_customer)
+
+	move_all_customers()
+
 # transform 3d position with main camera to 2d position on screen
 func move_all_customers():
 	for customer in $Customers.get_children():
 		customer.move_two_unit()
-
-func _on_OrderMenu_confirmed():
-	$Customers.remove_child($Customers.get_child(0))
-	move_all_customers()
 
 func _on_DishesPopup_id_pressed(menu_id):
 	make_dish(menu_id)
@@ -61,6 +67,7 @@ func make_dish(menu_id):
 	print("make dish " + str(menu_id))
 
 	# display progress bar
+
 
 ################
 # Utils functions
